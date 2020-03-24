cb01a0ea43034c27622656ec5973a466db95ebdb
s.omi <s.omi@iqnet.co.jp>
2020-03-24 16:24:09 +0900

    概要文面作成 不要なソースコードの削除 #23

--- my-nuxt-app/pages/index.vue
+++ my-nuxt-app/pages/index.vue
@@ -1,58 +1,143 @@
 <template>
   <section class="container">
-    <login v-if="!auth" />
-    <div v-if="auth">
-      {{ test }}
-      <app-logo />
-      <div id="app">
-        <h1 class="title">
-          アプリ名
-        </h1>
-        <input v-model="newItem" type="text" />
-        <form @submit.prevent>
-          <button @click="addItem">add</button>
-        </form>
-        <li v-for="(todo, key) in todos" :key="key">
-          {{ todo.item }}
-        </li>
-        <h2 class="subtitle">
-          アプリ名
-        </h2>
-        <nuxt-link to="/test">new cout page</nuxt-link><br />
-      </div>
+    <div id="app">
+      <el-row class="main">
+        <br />
+        <el-col :span="4">
+          <br />
+          <br />
+          <br />
+          <br />
+          <el-menu default-active="2" class="el-menu-vertical-demo">
+            <transition name="component-fade" mode="out-in">
+              <div v-if="auth" key="login">
+                <el-submenu index="help">
+                  <template slot="title">
+                    <i class="el-icon-document" />
+                    <span>助けて！！</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1" @click="getPosts">
+                      みんなの助けて！！
+                    </el-menu-item>
+                    <el-menu-item
+                      index="1-2"
+                      @click="getPostsByUserId(loginId)"
+                    >
+                      あなたの助けて！！
+                    </el-menu-item>
+                    <el-menu-item index="1-3" @click="getTodoList">
+                      あなたが助ける！！
+                    </el-menu-item>
+                  </el-menu-item-group>
+                </el-submenu>
+                <el-menu-item index="point" @click="getPoints">
+                  <i class="el-icon-ice-cream-round" />
+                  ポイント履歴
+                </el-menu-item>
+                <el-menu-item index="about" @click="nowMain = 'About'">
+                  <i class="el-icon-info" />
+                  どんなサービス？
+                </el-menu-item>
+                <el-menu-item index="logout" @click="logout">
+                  <i class="el-icon-back" />
+                  <span>ログアウト</span>
+                </el-menu-item>
+              </div>
+              <div v-else key="logout">
+                <el-menu-item
+                  v-if="!auth"
+                  key="login"
+                  index="login"
+                  @click="login"
+                >
+                  <i class="el-icon-right" />
+                  <span>ログイン</span>
+                </el-menu-item>
+                <el-menu-item index="about" @click="nowMain = 'About'">
+                  <i class="el-icon-info" />
+                  どんなサービス？
+                </el-menu-item>
+              </div>
+            </transition>
+          </el-menu>
+        </el-col>
+        <el-col :span="20">
+          <transition name="component-fade" mode="out-in">
+            <component
+              :is="nowMain"
+              :data="mainData"
+              @getPostsByUserId="getPostsByUserId"
+            />
+          </transition>
+        </el-col>
+      </el-row>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from "~/components/AppLogo.vue"
 import Login from "~/components/Login.vue"
+import PostMain from "~/components/posts/Main.vue"
+import PointMain from "~/components/points/Main.vue"
+import About from "~/components/About.vue"
+import axios from "axios"
 
 export default {
   components: {
-    AppLogo,
-    Login
+    Login,
+    PostMain,
+    PointMain,
+    About
   },
   data() {
     return {
-      newItem: "",
-      todos: []
+      mainData: null,
+      nowMain: "About",
+      isCollapse: true
     }
   },
   computed: {
-    test() {
-      return process.env.userUrl
-    },
     auth() {
       return this.$store.state.users.auth
+    },
+    loginId() {
+      return this.$store.state.users.loginId
     }
   },
   methods: {
-    addItem: function(event) {
-      var todo = {
-        item: this.newItem
-      }
-      this.todos.push(todo)
+    getPosts: async function(event) {
+      var response = await axios.get(process.env.postUrl + "/posts")
+      console.log(response)
+      this.nowMain = "PostMain"
+      this.mainData = response.data
+    },
+    getPostsByUserId: async function(id, name) {
+      var response = await axios.get(process.env.postUrl + "/user/" + id)
+      this.nowMain = "PostMain"
+      this.mainData = response.data
+    },
+    getTodoList: async function(event) {
+      var response = await axios.get(
+        process.env.postUrl + "/helper/" + this.$store.state.users.loginId
+      )
+      console.log(response)
+      this.nowMain = "PostMain"
+      this.mainData = response.data
+    },
+    getPoints: async function(event) {
+      var response = await axios.get(
+        process.env.pointUrl + "/points/" + this.$store.state.users.loginId
+      )
+      this.nowMain = "PointMain"
+      this.mainData = response.data
+    },
+    login() {
+      this.nowMain = "Login"
+    },
+    logout() {
+      this.nowMain = "Login"
+      this.$store.dispatch("users/logout")
     }
   }
 }
@@ -60,32 +145,24 @@ export default {
 
 <style>
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.component-fade-enter,
+.component-fade-leave-to {
+  transform: translateX(-20px);
+  opacity: 0;
 }
-
-.links {
-  padding-top: 15px;
+.button-fade-enter-active,
+.button-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.button-fade-enter,
+.button-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
 }
 </style>

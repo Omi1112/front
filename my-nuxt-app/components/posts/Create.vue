4157a659deb58b1b211642a557fa7d30095bfe04
s.omi <s.omi@iqnet.co.jp>
2020-04-01 18:00:05 +0900

    一旦実装 未検証

--- my-nuxt-app/components/posts/Create.vue
+++ my-nuxt-app/components/posts/Create.vue
@@ -14,6 +14,11 @@
           支払いポイント<br />
           （支払い可能ポイント：{{ maxPoint | addComma }}ポイント）
           <el-slider v-model="point" :max="maxPoint" :step="10" show-input />
+          <el-input v-model="tag" type="text" />
+          <el-button type="primary" @click="addTag(tag)">タグ追加</el-button>
+          <span v-for="(tag, key) in suggestTagList" :key="key" :data-index="key">
+            <el-button type="text" @click="addTag(tag.body)">{{tag.body}}</el-button>
+          </span>
           <el-button type="primary" native-type="submit">送信</el-button>
         </form>
       </el-card>
@@ -32,12 +37,31 @@ export default {
       error: null,
       point: 0,
       maxPoint: 0,
-      body: ""
+      body: "",
+      tag: "",
+      inputTagList: [],
+      suggestTagList: [],
     }
   },
   computed: {
     loginId() {
       return this.$store.state.users.loginId
+    }
+  },
+  watch: {
+    async getTags(tag) {
+      if (tag !== "") {
+        try {
+          var response = await axios.get(
+            process.env.postUrl + "/tag/like/" + tag
+          )
+          this.suggestTagList = response.data
+        } catch (e) {
+          this.error = e.message
+        }
+      } else {
+          this.suggestTagList = []
+      }
     }
   },
   methods: {
@@ -46,6 +70,7 @@ export default {
         var response = await axios.post(process.env.postUrl + "/posts", {
           body: this.body,
           point: this.point,
+          tags: this.inputTagList,
           token: this.$store.state.users.token
         })
         this.body = ""
@@ -65,6 +90,9 @@ export default {
       } catch (e) {
         this.error = e.message
       }
+    },
+    async addTag(body) {
+      this.inputTagList.push({id: 0, body: body})
     }
   }
 }
--- my-nuxt-app/pages/index.vue
+++ my-nuxt-app/pages/index.vue
@@ -1,24 +1,6 @@
 <template>
   <section class="container">
     <div id="app">
-      <!-- <el-menu
-          default-active="activeIndex"
-          mode="horizontal"
-          class="el-menu-demo help-menu"
-        > -->
-      <!-- <transition name="component-fade" mode="out-in"> -->
-      <!-- <div v-if="auth" key="login"> -->
-      <!-- <el-row class="user"> -->
-      <!-- <el-col :span="2">&nbsp;</el-col> -->
-      <!-- <el-col :span="2"> -->
-      <!-- <i class="el-icon-user-solid" /> -->
-      <!-- </el-col>
-              <el-col :span="20"> -->
-      <!-- ようこそ{{ userName }}さん<br />
-          所持ポイント{{ userPoint }} -->
-      <!-- </el-col> -->
-      <!-- </el-row> -->
-      <!-- <el-row class="user"> -->
       <transition name="component-fade" mode="out-in">
         <el-menu
           v-if="auth"
@@ -35,13 +17,6 @@
               <i class="el-icon-user-solid" />
               ようこそ{{ userName }}さん
             </el-menu-item>
-            <!-- <el-menu-item-group>
-              <el-submenu index="help">
-                <template slot="title">
-                  <i class="el-icon-document" />
-                  <span>助けて！！</span>
-                </template>
-                <el-menu-item-group> -->
             <el-menu-item index="1-1" @click="getPosts">
               <i class="el-icon-document" />
               みんなの助けて！！
@@ -72,36 +47,6 @@
             <i class="el-icon-ice-cream-round" />
             所持ポイント{{ userPoint }}
           </el-menu-item>
-
-          <!-- <el-submenu index="help">
-            <template slot="title">
-              <i class="el-icon-document" />
-              <span>助けて！！</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1" @click="getPosts">
-                みんなの助けて！！
-              </el-menu-item>
-              <el-menu-item index="1-2" @click="getPostsByUserId(loginId)">
-                あなたの助けて！！
-              </el-menu-item>
-              <el-menu-item index="1-3" @click="getTodoList">
-                あなたが助ける！！
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-submenu> -->
-          <!-- <el-menu-item index="point" @click="getPoints">
-            <i class="el-icon-ice-cream-round" />
-            ポイント履歴
-          </el-menu-item>
-          <el-menu-item index="about" @click="nowMain = 'About'">
-            <i class="el-icon-info" />
-            どんなサービス？
-          </el-menu-item>
-          <el-menu-item index="logout" @click="logout">
-            <i class="el-icon-back" />
-            <span>ログアウト</span>
-          </el-menu-item> -->
         </el-menu>
 
         <el-menu
@@ -120,33 +65,6 @@
           </el-menu-item>
         </el-menu>
       </transition>
-
-      <!-- <transition name="component-fade" mode="out-in">
-        <el-menu-item v-if="auth" index="point" @click="getPoints">
-          <i class="el-icon-ice-cream-round" />
-          ポイント履歴
-        </el-menu-item>
-      </transition>
-      <el-menu-item
-        index="about"
-        class="help-menu-item"
-        @click="nowMain = 'About'"
-      >
-        <i class="el-icon-info" />
-        どんなサービス？
-      </el-menu-item>
-      <transition name="component-fade" mode="out-in">
-        <el-menu-item v-if="auth" index="logout" @click="logout">
-          <i class="el-icon-back" />
-          <span>ログアウト</span>
-        </el-menu-item>
-      </transition>
-      <transition name="component-fade" mode="out-in">
-        <el-menu-item v-if="!auth" key="login" index="login" @click="login">
-          <i class="el-icon-right" />
-          <span>ログイン</span>
-        </el-menu-item>
-      </transition> -->
 
       <transition name="component-fade" mode="out-in">
         <component :is="nowMain" />

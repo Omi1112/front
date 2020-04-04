7f63df06e949bf99759ea4ac4e8e891436e0468a
s.omi <omi1520135@gmail.com>
2020-04-04 20:30:50 +0900

    jest導入

---
+++ my-nuxt-app/spec/store/points.spec.js
@@ -0,0 +1,46 @@
+import Vuex from "vuex"
+import * as count from "@/store/points"
+import { createLocalVue } from "@vue/test-utils"
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let action
+const testedAction = (context = {}, payload = {}) => {
+  return count.actions[action](context, payload)
+}
+
+describe("store/points.js", () => {
+  const OLD_ENV = process.env
+  let store
+
+  beforeEach(() => {
+    jest.resetModules() // this is important - it clears the cache
+    process.env = { ...OLD_ENV }
+    delete process.env.NODE_ENV
+    store = new Vuex.Store(count)
+  })
+
+  afterEach(() => {
+    process.env = OLD_ENV
+  })
+
+  describe("actions", () => {
+    process.env.pointUrl = "http://point:9000"
+
+    let commit
+    let state
+    beforeEach(() => {
+      commit = store.commit
+      state = store.state
+    })
+    test("getPointsByUserId", async done => {
+      action = "getPointsByUserId"
+      console.log(store.state.points.length)
+      expect(store.state.points.length).toBe(0)
+      await testedAction({ commit, state }, 1)
+      expect(store.state.points.length).not.toBe(0)
+      done()
+    })
+  })
+})
